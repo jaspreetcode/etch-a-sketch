9bca54e9b2eecb5d0e2320e38fd2b6e2fc0af3e7
sukhpreet kaur <sukhpreetkaur@sukhpreets-MacBook-Air.local>
2025-01-15 10:00:39 +0530

    Complete the basic version of the project - Etch-a-Sketch

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
 const container = document.querySelector(".container");
+const popupButton = document.querySelector(".popup");
+
+let numberOfSquares = 16;
+
+function createGrid(numberOfSquares) {
+    for (i = 0; i < numberOfSquares * numberOfSquares; i++) {
+        const squareDiv = document.createElement("div");
+        squareDiv.classList.add("square");
+        squareDiv.style.width = (container.clientWidth / numberOfSquares) + "px";
+        squareDiv.style.height = (container.clientHeight / numberOfSquares) + "px";
+        container.appendChild(squareDiv);
+    }
+}
+
+createGrid(numberOfSquares);
+
+function openPopup() {
+    let userInput = prompt("Enter the value between 4 and 100: ");
+    if (userInput != null) {
+        numberOfSquares = parseInt(userInput);
+        container.innerHTML = "";
+        createGrid(numberOfSquares);
+        listen();
+    }
+}
+
+popupButton.addEventListener("click", openPopup);
+
+function listen() {
+    const squareDivs = document.querySelectorAll("div.square");
+
+    function addColor(e) {
+        e.target.style.backgroundColor = "yellow";
+    }
+
+    squareDivs.forEach(div => {
+        div.addEventListener("mouseover", addColor);
+    });
+}
+
+listen();
